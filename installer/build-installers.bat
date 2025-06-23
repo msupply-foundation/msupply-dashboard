5abaf31f9818f725a1444d06e2f3ea20a239b465
mark-prins <mark@malcolmprins.net>
2025-06-24 09:28:07 +1200

    path test

--- installer/build-installers.bat
+++ installer/build-installers.bat
@@ -4,15 +4,17 @@
 
 @ECHO.
 @ECHO ##### Adjusting SUFS #####
-SET installerWorkspace=C:\Program Files (x86)\Jenkins\jobs\mSupply Dashboard Installers\workspace
+@ECHO "Current working directory: %WORKSPACE%"
+@ECHO "Current working dir: %workspace_dir%"
+// SET installerWorkspace=C:\Users\Administrator\AppData\Local\Jenkins\.jenkins\workspace\mSupply Dashboard Installers
 FOR /F "delims=*" %%i in ('more version.txt') do SET versionTag=%%i
 @ECHO "current tag = %versionTag%"
 
 cd installer
-node "%installerWorkspace%\installer\adjustSUFs.js"
+node "%WORKSPACE%\installer\adjustSUFs.js"
 cd ..
 
 @ECHO.
 @ECHO ##### Creating installers #####
-start "" /wait "C:\Program Files (x86)\Setup Factory 9\SUFDesign.exe" /BUILD /LOG:installer\setup-factory.log "%installerWorkspace%\installer\dashboard.suf"
-start "" /wait "C:\Program Files (x86)\Setup Factory 9\SUFDesign.exe" /BUILD /LOG:installer\setup-factory.log "%installerWorkspace%\installer\dashboard-upgrade.suf"
+start "" /wait "C:\Program Files (x86)\Setup Factory 9\SUFDesign.exe" /BUILD /LOG:installer\setup-factory.log "%WORKSPACE%\installer\dashboard.suf"
+start "" /wait "C:\Program Files (x86)\Setup Factory 9\SUFDesign.exe" /BUILD /LOG:installer\setup-factory.log "%WORKSPACE%\installer\dashboard-upgrade.suf"
